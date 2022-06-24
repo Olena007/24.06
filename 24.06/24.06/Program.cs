616454f83f86590f59cf95cb26da4b693003926c
lena <llogvinovaaaa@gmail.com>
2022-06-25 00:51:49 +0200

    24/06

---
+++ 24.06/24.06/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _24._06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Services.JSONReader reader = new Services.JSONReader();
+            //reader.Reader();
+
+            Services.Starter starter = new Services.Starter();
+
+            starter.FillEvent += starter.handler;
+            starter.FillEvent2 += starter.handler;
+
+            starter.Open();
+        }
+    }
+}
---
+++ 24.06/24.06/Services/ActionServer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._06.Services
+{
+    internal class ActionServer
+    {
+        private DateTime date = DateTime.Now;
+
+        public async Task FillFile()
+        {
+            
+        }
+
+    }
+}
